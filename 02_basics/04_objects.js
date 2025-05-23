61585e030f1da60e878554d596d97af9bf29a3a5
addyname <madnanahmad8698@gmail.com>
2025-05-23 11:17:23 +0000

    04_objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,3 +1,25 @@
 // const tinderUser = new Object() // Singleton Object
 const tinderUser = {}  // Non Singleton Object
-console.log(tinderUser)
+
+tinderUser.id = "123"
+tinderUser.email = "test@gmail.com"
+tinderUser.phone = "1234567890"
+
+// console.log(tinderUser)
+
+const myInformation = {
+    fullname: {
+        myfullname: {
+            firstname: "Adnan",
+            lastname: "Ahmad",
+        }
+    },
+    lastupdateName: "Ahmad",
+    email: [tinderUser.email],
+    address: "Naya Bazar Kamptee",
+}
+
+const information = function() {
+    console.log(`My full name is ${myInformation.fullname.myfullname.firstname} ${myInformation.lastupdateName}. My email id is ${myInformation.email} and my address is ${myInformation.address}.`)
+}
+console.log(information())

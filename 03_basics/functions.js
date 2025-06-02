18befb2f6e7fea5a3722403cb126fcecc89dda4b
addyname <madnanahmad8698@gmail.com>
2025-06-02 10:50:16 +0000

    fun

---
+++ 03_basics/functions.js
@@ -0,0 +1,35 @@
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+                      // Parameters//
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+//              //Arguments//
+// addTwoNumbers( 3 , 6 )
+
+// function myNumber(number1, number2){
+//     console.log(number1+number2)
+// }
+// const myNewNumber = myNumber(3,4)
+
+// console.log("Number:", myNewNumber) // Give value undefined, so we use below method
+
+function myNumber(number1, number2){
+    console.log(number1+number2)
+    return number1+number2
+}
+const myNewNumber = myNumber(3,4)
+
+console.log("Number:", myNewNumber) // Use "return", if we use variable.
+
+
+

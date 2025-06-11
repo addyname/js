e77198aa9131d5c64fcbd1470c99e37955fda08d
addyname <madnanahmad8698@gmail.com>
2025-06-11 07:11:46 +0000

    fun

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -40,3 +40,32 @@ function loginUserMessage(username = "User"){ // Use default value
 }
 console.log(loginUserMessage())
 
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));

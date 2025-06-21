1e8b0f312bad29dae0b80c054d9c393e85fd64fa
addyname <madnanahmad8698@gmail.com>
2025-06-21 06:22:59 +0000

    life

---
+++ 03_basics/iife.js
@@ -0,0 +1,16 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai() {
+    // named IIFE
+    console.log(`DB Connected`)
+}) ();
+
+// Add ";" to work the (IIFE) function. Without it is showing error.
+
+(() => {
+    console.log(`DB Connected Two`)
+}) ();
+
+((name) => {
+    console.log(`DB Connected Three ${name}`)
+}) ("Adnan");

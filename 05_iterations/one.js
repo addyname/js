a6bc27fffe5b5cfd9a07a053151a1155eab19c90
addyname <madnanahmad8698@gmail.com>
2025-06-24 04:18:18 +0000

    for

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -10,10 +10,34 @@ for (let i = 1; i <= 7; i++) {
 }
 
 for (let i = 1; i <=5 ; i++) {
-    console.log(`Outer loop: ${i}`)
+    // console.log(`Outer loop: ${i}`)
     for (let j = 1; j <=5 ; j++) {
-        console.log(`${i} * ${j} = ${i * j}`)
+        // console.log(`${i} * ${j} = ${i * j}`)
         // console.log(i + '*' + j + ' = ' + i*j );
     }
     
+}
+
+let myArray = ["Adnan", "Seemab", "Monis", "Nikhat", "Ravi"]
+for (let a = 0; a < myArray.length; a++) {
+    const number = myArray[a]
+    // console.log(number)
+}
+
+// Break and Continue
+
+for (let b = 1; b <= 10; b++) {
+    if (b == 7) {
+        // console.log("Stop on number 7.")
+        break;
+    }
+    // console.log(`Value is ${b}.`)
+}
+
+for (let c = 1; c <= 10; c++) {
+    if (c == 7) {
+        console.log("Stop on number 7.")
+        continue;
+    }
+    console.log(`Value is ${c}.`)
 }

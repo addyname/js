2754e34252809f2059a3e50d3b59dfa57ec405fd
addyname <madnanahmad8698@gmail.com>
2025-06-24 05:03:18 +0000

    two

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+let w = 1
+while (w < 10) {
+    // console.log(`Value is ${w}`)
+    w = w + 2
+}
+
+let myArray = ["Adnan", "Seemab", "Monis", "Nikhat", "Ravi"]
+let a = 0
+while (a < myArray.length) {
+    // console.log(`${myArray[a]}`);
+    a = a + 1
+}
+
+let score = 1
+do {
+    // console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
+
+let Score = 11
+do {
+    console.log(`Score is ${Score}`);
+    Score++
+} while (Score <= 10);

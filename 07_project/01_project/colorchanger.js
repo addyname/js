9c43433b7ec86578d77c6930fae63f0153963616
addyname <madnanahmad8698@gmail.com>
2025-07-16 11:13:16 +0000

    project

---
+++ 07_project/01_project/colorchanger.js
@@ -0,0 +1,40 @@
+const allButton = document.querySelectorAll(".button, .reset-color");
+const body = document.querySelector("body");
+const textColor = document.querySelector(".change-color")
+
+allButton.forEach((button) => {
+  button.addEventListener("click", (clicked) => {
+    switch (clicked.target.id) {
+      case "grey":
+        body.style.backgroundColor = clicked.target.id;
+        break;
+
+      case "green":
+        body.style.backgroundColor = clicked.target.id;
+        break;
+
+      case "blue":
+        body.style.backgroundColor = clicked.target.id;
+        break;
+
+      case "yellow":
+        body.style.backgroundColor = clicked.target.id;
+        break;
+
+      case "text-color":
+        textColor.style.color = "#fff";
+        textColor.style.backgroundColor = "purple";
+        break;
+
+      case "reset":
+        body.style.backgroundColor = "initial";
+        textColor.style.color = "initial";
+        textColor.style.backgroundColor = "initial";
+        break;
+
+      default:
+        console.log("No color found.");
+        break;
+    }
+  });
+});

887943e8431337b1664f95ae3dd5846aef20e888
addyname <madnanahmad8698@gmail.com>
2025-07-18 05:38:34 +0000

    pro4

--- 07_project/01_project/colorchanger.js
+++ 07_project/01_project/colorchanger.js
@@ -1,6 +1,15 @@
-const allButton = document.querySelectorAll(".button, .reset-color");
+const allButton = document.querySelectorAll(
+  ".button, .reset-color, .change-text-color, .change-rondom-color"
+);
 const body = document.querySelector("body");
 const textColor = document.querySelectorAll(".change-color"); // We have to add loop for working this on all heading classes. Because this won't work on a NodeList.
+const changeRanColor = document.querySelectorAll(".random-color");
+
+const randomColor = ["red", "green", "pink"];
+function changeToRandom() {
+  ranColor = Math.floor(Math.random() * randomColor.length);
+  return randomColor[ranColor];
+}
 
 allButton.forEach((button) => {
   button.addEventListener("click", (clicked) => {
@@ -29,13 +38,20 @@ allButton.forEach((button) => {
       //   textColor.style.color = "initial";
       //   textColor.style.backgroundColor = "initial";
       //   break; // We have to add loop for working this on all heading classes. Because this won't work on a NodeList.
-      
+
       case "text-color":
         textColor.forEach((heading) => {
           heading.style.color = "#fff";
           heading.style.backgroundColor = "purple";
         });
         break;
+
+        case "RandomColor":
+       changeRanColor.forEach((rantext) => {
+          rantext.style.color = changeToRandom()
+        })
+        break;
+
       case "reset":
         body.style.backgroundColor = "initial";
         textColor.forEach((headingReset) => {
@@ -43,10 +59,10 @@ allButton.forEach((button) => {
           headingReset.style.backgroundColor = "initial";
         });
         break;
+
       default:
         console.log("No color found.");
         break;
-        
     }
   });
 });

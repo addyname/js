19f27970f1d3a8c13ddee7811d87857549607157
addyname <madnanahmad8698@gmail.com>
2025-07-21 10:24:34 +0000

    pro7

--- 07_project/02_project/calculator.js
+++ 07_project/02_project/calculator.js
@@ -1,23 +1,56 @@
-function calculateProtein() {
-      const gender = document.getElementById("gender").value;
-      const heightCm = parseFloat(document.getElementById("height").value);
+const form = document.querySelector("form");
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
 
-      if (isNaN(heightCm) || heightCm <= 0) {
-        document.getElementById("result").textContent = "Please enter a valid height.";
-        return;
-      }
+form.addEventListener("submit", function (s) {
+  s.preventDefault();
 
-      const heightInches = heightCm / 2.54;
-      let ibw;
+  const gender = document.querySelector('#gender').value
+  const mHeight = parseInt(document.querySelector("#height").value);
+  const mWeight = parseInt(document.querySelector("#weight").value);
+  const result = document.querySelector("#result");
+  const activity = document.getElementById('activity').value
 
-      if (gender === "male") {
-        ibw = 50 + 2.3 * (heightInches - 60);
-      } else {
-        ibw = 45.5 + 2.3 * (heightInches - 60);
-      }
+    if (isNaN(mHeight) || mHeight <= 0 || isNaN(mWeight) || mWeight <= 0) {
+    result.innerHTML = `<span>Please enter valid height and weight.</span>`;
+    return;
+  }
+  if (mHeight < mWeight) {
+    result.innerHTML = `<span>Height should be greater than the weight.</span>`;
+    return;
+  }
 
-      const proteinGrams = Math.round(ibw);
+  const heightInInches = mHeight / 2.54;
+  
+let calculateWeight;
+  if(gender === "male") {
+    calculateWeight = 50 + 2.3 * (heightInInches - 60);
+  } else {
+    calculateWeight = 45.5 + 2.3 * (heightInInches - 60);
+  }
 
-      document.getElementById("result").textContent = 
-        `Your ideal daily protein intake is about ${proteinGrams} grams.`;
-    }
+  let finalWeight;
+  if (mWeight > calculateWeight) {
+    finalWeight = calculateWeight + 0.25 * (mWeight - calculateWeight);
+  } else {
+    finalWeight = mWeight;
+  }
+
+  let trainingIntensity;
+  if (activity === 'light') {
+    trainingIntensity = 1.3;
+  }else if (activity === 'medium') {
+    trainingIntensity = 1.5;
+  }else if (activity === "hard") {
+    trainingIntensity = 1.8;
+  }
+
+  /*Ternary Operator (shorter)*/ 
+  // const finalWeight = mWeight > calculateheight 
+  // ? calculateheight + 0.25 * (mWeight - calculateheight) 
+  // : mWeight; 
+
+  const proteinGrams = Math.round(finalWeight * trainingIntensity);
+
+  result.innerHTML = `<span>You need approximately <strong>${proteinGrams} grams</strong> of protein per day.</span>`;
+});
